08104702a8a15dc7ae11bb911e2097968e82e134
真昼 <2071595839@qq.com>
2026-08-26 00:41:44 +0800

    Add LeetCode 128 longest-consecutive-sequence

---
+++ leetcode/0128_longest-consecutive-sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(), nums.end());
+        int ans = 0;
+
+        for (int x : s) {
+            if (s.count(x - 1)) continue;
+
+            int len = 1;
+            while (s.count(x + len)) {
+                len++;
+            }
+            ans = max(ans, len);
+        }
+
+        return ans;
+    }
+};
